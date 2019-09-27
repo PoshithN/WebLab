32a6ce2e6c861390505d03038191e3cddb13e54f
PoshithN <53933152+PoshithN@users.noreply.github.com>
2019-09-27 11:53:27 +0530

    Update prog7.php

--- prog7.php
+++ prog7.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set("Asia/Kolkata");
-echo "<p>".date("d/m/y::h:i:s A")."</p>";
+echo "<p>".date("d/m/y::h:i:s A")."</p>";#do not use p/P with 'h'
 echo "<p>".date("d/m/y::h:i:sa")."</p>";
 echo "<p>".date("d/m/y::H:i:s")."</p>";
 echo "<p>".date("d/m/y::H:i:s P")."</p>";
