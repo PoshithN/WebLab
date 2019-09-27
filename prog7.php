85f399bdb4e4ffe8e68631bc6faa296d60563065
PoshithN <53933152+PoshithN@users.noreply.github.com>
2019-09-27 11:50:54 +0530

    Add files via upload

---
+++ prog7.php
@@ -0,0 +1,8 @@
+<?php
+date_default_timezone_set("Asia/Kolkata");
+echo "<p>".date("d/m/y::h:i:s A")."</p>";
+echo "<p>".date("d/m/y::h:i:sa")."</p>";
+echo "<p>".date("d/m/y::H:i:s")."</p>";
+echo "<p>".date("d/m/y::H:i:s P")."</p>";
+echo "<p>".date("d/m/y::H:i:s a")."</p>";
+?>
