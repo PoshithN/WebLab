56a28d8367ba96eeda67580d76ab98069c4f4570
PoshithN <53933152+PoshithN@users.noreply.github.com>
2019-09-27 11:52:21 +0530

    Update prog6.php

--- prog6.php
+++ prog6.php
@@ -1,6 +1,6 @@
 <?php
-$fp = fopen("counterlog.txt", "r");
-$count = fread($fp,10);
+$fp = fopen("counterlog.txt", "r");#any NAME with .txt extension
+$count = fread($fp,10); #10 indicates number of bytes(any value)
 fclose($fp);
 $count = $count+1;
 echo "<p> Page Views: " .$count. "</p>";
