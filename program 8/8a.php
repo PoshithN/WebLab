b3ef3257f8b3545dd4381315e3826a589c8b8f4a
PoshithN <53933152+PoshithN@users.noreply.github.com>
2019-10-24 19:55:50 +0530

    Add files via upload

--- program 8/8a.php
+++ program 8/8a.php
@@ -11,8 +11,8 @@ if(isset($_POST['but']))
 </head>
 <body>
 <style>
-input[type="button"]{width:100%;height:100%}
-input[type="submit"]{width:100%;height:100%}
+input[type="button"]{width:100%;height:100%;color: white; background-color:black;}
+input[type="submit"]{width:100%;height:100%;color: white; background-color:black;}
 table{width:50%;height:50%}
 input[type="text"]{width:100%;height:100%}
 </style>
@@ -23,28 +23,28 @@ input[type="text"]{width:100%;height:100%}
 <td  colspan=4><input type="text" name="disp" value="<?php if(isset($res)) echo $res; ?>"></td>
 </tr>
 <tr>
-<td><input style="color: white; background-color:black;"type="button"value="1" onclick="cal.disp.value += '1' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="2" onclick="cal.disp.value += '2' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="3" onclick="cal.disp.value += '3' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="*" onclick="cal.disp.value += '*' "></td>
+<td><input type="button"value="1" onclick="cal.disp.value += '1' "></td>
+<td><input type="button"value="2" onclick="cal.disp.value += '2' "></td>
+<td><input type="button"value="3" onclick="cal.disp.value += '3' "></td>
+<td><input type="button"value="*" onclick="cal.disp.value += '*' "></td>
 </tr>
 <tr>
-<td><input style="color: white; background-color:black;"type="button"value="4" onclick="cal.disp.value += '4' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="5" onclick="cal.disp.value += '5' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="6" onclick="cal.disp.value += '6' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="/" onclick="cal.disp.value += '/' "></td>
+<td><input type="button"value="4" onclick="cal.disp.value += '4' "></td>
+<td><input type="button"value="5" onclick="cal.disp.value += '5' "></td>
+<td><input type="button"value="6" onclick="cal.disp.value += '6' "></td>
+<td><input type="button"value="/" onclick="cal.disp.value += '/' "></td>
 </tr>
 <tr>
-<td><input style="color: white; background-color:black;"type="button"value="7" onclick="cal.disp.value += '7' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="8" onclick="cal.disp.value += '8' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="9" onclick="cal.disp.value += '9' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="-" onclick="cal.disp.value += '-' "></td>
+<td><input type="button"value="7" onclick="cal.disp.value += '7' "></td>
+<td><input type="button"value="8" onclick="cal.disp.value += '8' "></td>
+<td><input type="button"value="9" onclick="cal.disp.value += '9' "></td>
+<td><input type="button"value="-" onclick="cal.disp.value += '-' "></td>
 </tr>
 <tr>
-<td><input style="color: white; background-color:black;"type="button"value="c" onclick="cal.disp.value = ' ' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="0" onclick="cal.disp.value += '0' "></td>
-<td><input style="color: white; background-color:black;"type="button"value="+" onclick="cal.disp.value += '+' "></ts>
-<td><input style="color: white; background-color:black;"type="submit"value="=" name="but"></td>
+<td><input type="button"value="c" onclick="cal.disp.value = ' ' "></td>
+<td><input type="button"value="0" onclick="cal.disp.value += '0' "></td>
+<td><input type="button"value="+" onclick="cal.disp.value += '+' "></ts>
+<td><input type="submit"value="=" name="but"></td>
 </tr>
 </table>
 </center>
