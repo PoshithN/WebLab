f994c3e04f0664a23f1b56e7f04edbb78589abf2
PoshithN <53933152+PoshithN@users.noreply.github.com>
2019-11-24 16:07:54 +0530

    Add files via upload

--- prog7.php
+++ prog7.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("Asia/Kolkata");
-echo "<p>".date("d/m/y::h:i:s A")."</p>";#do not use 'P' with 'h'
-echo "<p>".date("d/m/y::h:i:sa")."</p>";
+echo "date & time:".date("d/m/y::h:i:s A");#do not use 'P' with 'h'
+echo "<p>".date("d/m/y::h:i:sa")."</p>";#'m' always stands for month.. do not use in place of minute ('i')
 echo "<p>".date("d/m/y::H:i:s")."</p>";
 echo "<p>".date("d/m/y::H:i:s P")."</p>";#do not use 'p'.... can use 'P'
 echo "<p>".date("d/m/y::H:i:s a")."</p>";#no problem to use 'a'/'A'
